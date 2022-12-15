80f39de0e67dfaea788d6bb1e23bea04401693a2
leviNzamba <levinzamba@gmail.com>
2022-12-15 16:36:55 +0300

    initial

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,14 +16,7 @@ const config = {
 		  postcss: true,
 		}),
 	],
-	vite: {
-		resolve: {
-			alias: {
-				$components: resolve('./src/components'),
-				$store: resolve('./src/store'),
-			}
-		}
-	},
+	
 };
 
 export default config;
